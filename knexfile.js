be149c54537ea6223c23f324ea7d688d00a4251b
pralhad88 <pralhad18@navgurukul.org>
2021-12-21 20:07:49 +0530

    hosted database url config

--- knexfile.js
+++ knexfile.js
@@ -27,7 +27,9 @@ module.exports = {
       host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
-      password: process.env.DB_PASS
+      password: process.env.DB_PASS,
+      port: 5432,
+      ssl: true
     },
     pool: {
       min: 2,
@@ -46,7 +48,9 @@ module.exports = {
       host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
-      password: process.env.DB_PASS
+      password: process.env.DB_PASS,
+      port: 5432,
+      ssl: true
     },
     pool: {
       min: 2,
